1ff6cb1eb68d867510aa21ece0a512048d7781f6
Medy <meronhailekiros@gmail.com>
2021-04-13 15:46:34 -0400

    Updates

---
+++ precache-manifest.323f059654a5e1d4da1802e16648bba5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ad23f75d0ac499136afaaf8afa5f25c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e9202a2b143507cfc9b",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "94833f0d9aaa3be6210f",
+    "url": "/static/js/2.19e99e5d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.19e99e5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e9202a2b143507cfc9b",
+    "url": "/static/js/main.55ee625b.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "39fed7e637b80886a5999b1eaf1d8ddd",
+    "url": "/static/media/resume.39fed7e6.pdf"
+  }
+]);
